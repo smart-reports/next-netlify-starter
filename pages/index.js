c33c90d40a316f1f9a35f39a6264cab358beddca
smart-reports <50381700+smart-reports@users.noreply.github.com>
2021-11-29 18:56:28 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>SPICE UK INTERGROUP BILLING AND COMMISSIONS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to your new module!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
